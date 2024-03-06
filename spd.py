c7aadd67a5911eae9b65a55cf7e20502a51d6e1c
Sourabh Dey <146729006+CodeRreaper69@users.noreply.github.com>
2024-03-06 20:50:30 +0530

    Update spd.py

--- spd.py
+++ spd.py
@@ -2,16 +2,21 @@ import os
 
 print("WELCOME TO OFFLINE TEXT TO SPEECH RECOGNITION")
 print("\n")
-while True:
-      n = input("""Enter the text to be spoken\n
-(Enter underscore '_' instead of space e.g- "Hi_World") :\n
-""")
-      r = int(input("Enter Rate of speech (-100 to 100) : "))
-      p = int(input("Enter pitch rate(-100 to 100) : "))
-      os.system(f"spd-say {n} -r {r} -p {p}")
-      c = input("Continue? (y/n):")
-      if c in ["y","Y"]:
-         pass
-      else:
-         exit()
 
+while True:
+    n = input("""Enter the text to be spoken
+(Separated with spaces):\n
+""")
+    # Replacing spaces with underscores
+    n = n.replace(" ", "_")
+
+    r = int(input("Enter Rate of speech (-100 to 100) : "))
+    p = int(input("Enter pitch rate(-100 to 100) : "))
+
+    os.system(f"spd-say {n} -r {r} -p {p}")
+
+    c = input("Continue? (y/n):")
+    if c.lower() in ["y", "yes"]:
+        pass
+    else:
+        exit()
