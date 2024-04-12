1ab766e7836d2da98c5d2a73cc6ea4f1334fecc6
Sourabh Dey <146729006+CodeRreaper69@users.noreply.github.com>
2024-04-13 01:50:00 +0530

    Add files via upload

---
+++ req.sh
@@ -0,0 +1,6 @@
+sudo apt install spd -y
+sudo apt install speech-dispatcher -y
+sudo snap install figlet -y
+sudo apt install figlet -y
+sudo apt install python3
+
---
+++ req_no.sh
@@ -0,0 +1,6 @@
+apt install spd -y
+apt install speech-dispatcher -y
+snap install figlet -y
+apt install figlet -y
+apt install python3
+
